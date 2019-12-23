30220e9d09ad2d7a2082b5d858a35cd59acad57d
th3N0m4d <edielton.dantas@hotmail.com>
2019-12-23 12:31:01 +0100

    Create the TotalRatings skeleton component

---
+++ src/Components/Products/Details/TotalRatings.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const TotalRatings = () => (
+  <div class='row total_rate'>
+    <div class='col-6'>
+      <div class='box_total'>
+        <h5>Overall</h5>
+        <h4>4.0</h4>
+        <h6>(03 Reviews)</h6>
+      </div>
+    </div>
+    <div class='col-6'>
+      <div class='rating_list'>
+        <h3>Based on 3 Reviews</h3>
+        <ul class='list'>
+          <li>
+            <a href='#'>5 Star
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' /> 01
+            </a>
+          </li>
+          <li>
+            <a href='#'>4 Star
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' /> 01
+            </a>
+          </li>
+          <li>
+            <a href='#'>3 Star
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' /> 01
+            </a>
+          </li>
+          <li>
+            <a href='#'>2 Star
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' /> 01
+            </a>
+          </li>
+          <li>
+            <a href='#'>1 Star
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' />
+              <i class='fa fa-star' /> 01
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
+
+export default TotalRatings
