d3e0863d3ebf743756bb506fc6a5bd5ccedeab4c
th3N0m4d <edielton.dantas@hotmail.com>
2019-12-25 05:35:42 +0100

    Create basic LoginForm stories

--- src/Components/Auth/LoginForm.jsx
+++ src/Components/Auth/LoginForm.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 
-const LoginForm = () => (
-  <div className='login_form_inner'>
-    <h3>Log in to enter</h3>
-    <form className='row login_form' action='contact_process.php' method='post' id='contactForm' novalidate='novalidate'>
-      <div className='col-md-12 form-group'>
-        <input type='text' className='form-control' id='name' name='name' placeholder='Username' />
+const propTypes = {
+  onSubmit: PropTypes.func
+}
+
+class LoginForm extends PureComponent {
+  state = {
+    username: '',
+    password: '',
+    keepLoggedIn: false
+  }
+
+  handleOnInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleOnSubmit = e => {
+    this.props.onSubmit(this.state)
+    e.preventDefault()
+  }
+
+  render () {
+    return (
+      <div className='login_form_inner'>
+        <h3>Log in to enter</h3>
+        <form className='row login_form' noValidate onSubmit={this.handleOnSubmit}>
+          <div className='col-md-12 form-group'>
+            <input type='text' className='form-control' name='username' placeholder='Username' onChange={this.handleOnInput} />
+          </div>
+          <div className='col-md-12 form-group'>
+            <input type='text' className='form-control' name='password' placeholder='Password' onChange={this.handleOnInput} />
+          </div>
+          <div className='col-md-12 form-group'>
+            <div className='creat_account'>
+              <input type='checkbox' id='f-option2' name='keepLoggedIn' onChange={this.handleOnInput} />
+              <label htmlFor='f-option2'>Keep me logged in</label>
+            </div>
+          </div>
+          <div className='col-md-12 form-group'>
+            <button type='submit' value='submit' className='btn submit_btn'>Log In</button>
+            <a href='/'>Forgot Password?</a>
+          </div>
+        </form>
       </div>
-      <div className='col-md-12 form-group'>
-        <input type='text' className='form-control' id='name' name='name' placeholder='Password' />
-      </div>
-      <div className='col-md-12 form-group'>
-        <div className='creat_account'>
-          <input type='checkbox' id='f-option2' name='selector' />
-          <label for='f-option2'>Keep me logged in</label>
-        </div>
-      </div>
-      <div className='col-md-12 form-group'>
-        <button type='submit' value='submit' className='btn submit_btn'>Log In</button>
-        <a href='#'>Forgot Password?</a>
-      </div>
-    </form>
-  </div>
-)
+    )
+  }
+}
+
+LoginForm.propTypes = propTypes
 
 export default LoginForm
