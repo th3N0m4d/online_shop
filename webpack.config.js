059237aa8192848ac1a1f7a7cc91d3eeae9b2a9f
th3N0m4d <edielton.dantas@hotmail.com>
2020-02-13 20:00:56 -0300

    Add module alias for images and remove bable.config file

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,32 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const APP_DIR = path.join(__dirname, 'src')
-const DIST_DIR = path.join(__dirname, 'dist')
+const PUBLIC_DIR = path.join(__dirname, 'public')
+
+const resolve = {
+  alias: {
+    images: `${PUBLIC_DIR}/img`,
+    '@': APP_DIR
+  },
+  extensions: ['.js', '.jsx']
+}
+
+const devServer = {
+  open: true
+}
+
+const plugins = [
+  new HtmlWebPackPlugin({
+    template: `${APP_DIR}/index.html`,
+    favicon: `${APP_DIR}/img/favicon.png`
+  }),
+  new CleanWebpackPlugin()
+]
 
 const config = {
-  entry: {
-    app: `${APP_DIR}/index.jsx`
-  },
-  output: {
-    filename: '[name].[contenthash].js'
-  },
-  resolve: {
-    alias: {
-      Utilities: path.resolve(APP_DIR, 'utilities/'),
-      '@': APP_DIR
-    },
-    extensions: ['.js', '.jsx']
-  },
-  optimization: {
-    moduleIds: 'hashed',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
-  },
-  devServer: {
-    contentBase: DIST_DIR,
-    port: 8080,
-    open: true
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: `${APP_DIR}/index.html`,
-      favicon: `${APP_DIR}/img/favicon.png`
-    }),
-    new CleanWebpackPlugin()
-  ],
+  resolve,
+  devServer,
+  plugins,
   module: {
     rules: [
       {
