d947ef5be8c397a715f38e5b3eb9ab202d2e2546
th3N0m4d <edielton.dantas@hotmail.com>
2020-02-13 20:06:49 -0300

    Move tests to __tests__ folder

--- __tests__/Footer.js
+++ __tests__/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import Footer from '.'
+import Footer from '@/components/Footer'
 
 describe('Footer', () => {
   it('should render', () => {
