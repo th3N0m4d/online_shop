f89bec9b850e1da460d14dc87215f8e69318ddf1
th3N0m4d <edielton.dantas@hotmail.com>
2020-02-16 19:42:31 -0300

    Improve code coverage on RegisterPage tests

--- __tests__/RegisterForm.js
+++ __tests__/RegisterForm.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+
 import RegisterForm from '@/components/Auth/RegisterForm'
-import * as R from 'ramda'
+import { triggerOnSubmit, triggerOnChange } from '@/utilities/helpers'
 
 describe('RegisterForm', () => {
   it('should render', () => {
@@ -14,54 +15,42 @@ describe('RegisterForm', () => {
 
   it('should trigger form submit', () => {
     const onSubmitSpy = jest.fn()
-    const once = 1
     const wrapper = shallow(
       <RegisterForm onSubmit={onSubmitSpy} />
     )
 
-    wrapper.find('form').simulate('submit', { preventDefault: R.always(undefined) })
+    const form = wrapper.find('form')
+
+    triggerOnSubmit(form)
 
     expect(onSubmitSpy).toHaveBeenCalled()
-    expect(onSubmitSpy).toHaveBeenCalledTimes(once)
   })
 
-  it('should submit form data', () => {
-    const onSubmitSpy = jest.fn()
-    const formData = {
-      name: 'John Doe',
-      email: 'john@gmail.com',
-      password: '1234',
-      password2: '1234',
-      keepLoggedIn: true
-    }
-    const wrapper = shallow(
-      <RegisterForm onSubmit={onSubmitSpy} />
-    )
+  it.each`
+    name            | value
+    ${'name'}            | ${'John Doe'} 
+    ${'email'}           | ${'john@gmail.com'}
+    ${'password'}        | ${'1234'}
+    ${'password2'}       | ${'1234'}
+    ${'keepLoggedIn'}    | ${true} 
+  
+  `('should trigger onChange callback for field "$name" with "$value"',
+    ({ name, value }) => {
+      const onChangeSpy = jest.fn()
+      const wrapper = shallow(
+        <RegisterForm onChange={onChangeSpy} />
+      )
+      const expectedParams = {
+        target: {
+          name,
+          value
+        }
+      }
 
-    wrapper.find('input[name="name"]').simulate('change', { target: { value: 'John Doe', name: 'name' } })
-    wrapper.find('input[name="email"]').simulate('change', { target: { value: 'john@gmail.com', name: 'email' } })
-    wrapper.find('input[name="password"]').simulate('change', { target: { value: '1234', name: 'password' } })
-    wrapper.find('input[name="password2"]').simulate('change', { target: { value: '1234', name: 'password2' } })
-    wrapper.find('input[name="keepLoggedIn"]').simulate('change', { target: { value: true, name: 'keepLoggedIn' } })
-    wrapper.find('form').simulate('submit', { preventDefault: R.always(undefined) })
+      const form = wrapper.find('form')
 
-    expect(onSubmitSpy).toHaveBeenLastCalledWith(formData)
-  })
+      triggerOnChange(form)({ name, value })
 
-  it('should not register new user when password does not match', () => {
-    const onSubmitSpy = jest.fn()
-
-    const wrapper = shallow(
-      <RegisterForm onSubmit={onSubmitSpy} />
-    )
-
-    wrapper.find('input[name="name"]').simulate('change', { target: { value: 'John Doe', name: 'name' } })
-    wrapper.find('input[name="email"]').simulate('change', { target: { value: 'john@gmail.com', name: 'email' } })
-    wrapper.find('input[name="password"]').simulate('change', { target: { value: '1234', name: 'password' } })
-    wrapper.find('input[name="password2"]').simulate('change', { target: { value: '12345', name: 'password2' } })
-    wrapper.find('input[name="keepLoggedIn"]').simulate('change', { target: { value: true, name: 'keepLoggedIn' } })
-    wrapper.find('form').simulate('submit', { preventDefault: R.always(undefined) })
-
-    expect(onSubmitSpy).not.toHaveBeenCalled()
-  })
+      expect(onChangeSpy).toHaveBeenLastCalledWith(expectedParams)
+    })
 })
--- src/Components/Auth/RegisterForm.jsx
+++ src/Components/Auth/RegisterForm.jsx
@@ -1,63 +1,40 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class RegisterForm extends PureComponent {
-  state = {
-    name: '',
-    email: '',
-    password: '',
-    password2: '',
-    keepLoggedIn: false
-  }
-
-  static propTypes = {
-    onSubmit: PropTypes.func
-  }
-
-  handleOnSubmit = e => {
-    if (this.state.password !== this.state.password2) {
-      return
-    }
-    this.props.onSubmit(this.state)
-    e.preventDefault()
-  }
-
-  handleOnChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  render () {
-    return (
-      <div className='login_form_inner reg_form'>
-        <h3>Create an Account</h3>
-        <form className='row login_form' noValidate onSubmit={this.handleOnSubmit}>
-          <div className='col-md-12 form-group'>
-            <input type='text' className='form-control' id='name' name='name' placeholder='Name' onChange={this.handleOnChange} />
-          </div>
-          <div className='col-md-12 form-group'>
-            <input type='email' className='form-control' id='email' name='email' placeholder='Email Address' onChange={this.handleOnChange} />
-          </div>
-          <div className='col-md-12 form-group'>
-            <input type='text' className='form-control' id='password' name='password' placeholder='Password' onChange={this.handleOnChange} />
-          </div>
-          <div className='col-md-12 form-group'>
-            <input type='password' className='form-control' id='password2' name='password2' placeholder='Confirm password' onChange={this.handleOnChange} />
-          </div>
-          <div className='col-md-12 form-group'>
-            <div className='creat_account'>
-              <input type='checkbox' id='keepLoggedIn' name='keepLoggedIn' onChange={this.handleOnChange} />
-              <label htmlFor='keepLoggedIn'>Keep me logged in</label>
-            </div>
-          </div>
-          <div className='col-md-12 form-group'>
-            <button type='submit' value='submit' className='btn submit_btn'>Register</button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+const propTypes = {
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func
 }
+
+const RegisterForm = ({ onChange, onSubmit }) => (
+  <div className='login_form_inner reg_form'>
+    <h3>Create an Account</h3>
+    <form className='row login_form' noValidate onSubmit={onSubmit}>
+      <div className='col-md-12 form-group'>
+        <input type='text' className='form-control' id='name' name='name' placeholder='Name' onChange={onChange} />
+      </div>
+      <div className='col-md-12 form-group'>
+        <input type='email' className='form-control' id='email' name='email' placeholder='Email Address' onChange={onChange} />
+      </div>
+      <div className='col-md-12 form-group'>
+        <input type='text' className='form-control' id='password' name='password' placeholder='Password' onChange={onChange} />
+      </div>
+      <div className='col-md-12 form-group'>
+        <input type='password' className='form-control' id='password2' name='password2' placeholder='Confirm password' onChange={onChange} />
+      </div>
+      <div className='col-md-12 form-group'>
+        <div className='creat_account'>
+          <input type='checkbox' id='keepLoggedIn' name='keepLoggedIn' onChange={onChange} />
+          <label htmlFor='keepLoggedIn'>Keep me logged in</label>
+        </div>
+      </div>
+      <div className='col-md-12 form-group'>
+        <button type='submit' value='submit' className='btn submit_btn'>Register</button>
+      </div>
+    </form>
+  </div>
+)
+
+RegisterForm.propTypes = propTypes
 
 export default RegisterForm
